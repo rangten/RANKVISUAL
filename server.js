3d552e28fe263b663fd55fc3422e3ac03edc831c
rangten <rangelfurlaneto@gmail.com>
2025-05-28 16:38:23 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "public")));
+// Servir arquivos estáticos da raiz do projeto
+app.use(express.static(__dirname));
 
+// Rota principal
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(PORT, () => {
